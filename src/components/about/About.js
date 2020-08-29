aa8c4938612cdef70b1679c49e66f264afcfef9a
NgTienToan <43052549+NgTienToan@users.noreply.github.com>
2020-08-29 14:31:41 +0700

    config router antd scss folder

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import React, { useState } from "react";
+=======
+<<<<<<< HEAD
+import React, {useState} from "react";
+>>>>>>> bf19f4f... config router antd scss folder
 import { Modal, Button } from "antd";
 import { useTranslation } from "react-i18next";
 
@@ -35,3 +40,53 @@ function About() {
   );
 }
 export default About;
+=======
+import React from "react";
+
+import { Modal, Button } from 'antd';
+
+class About extends React.Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Button type="primary" onClick={this.showModal}>
+          Open Modal
+        </Button>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </>
+    );
+  }
+}
+export default About;
+>>>>>>> a1c5b6a... config router antd scss folder
